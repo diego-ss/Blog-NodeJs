b858aba43b8f1ad381eb592ab11b214ed99cd176
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-27 12:44:08 -0300

    Configurando database e configurações iniciais do index.js

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 //carregando express
 const express = require("express");
 const app = express();
+//carregando body parser
+const bodyParser = require("body-parser");
+//carregando conexão
+const connection = require("./database/database");
+
+//setando view engine para ejs
+app.set("view engine", 'ejs');
+
+// Static itens estáticos na pasta public
+app.use(express.static("public"));
 
 //criando rota principal
 app.get("/", (req, res)=>{
-    res.send("Bem vindo!");
+    //renderizando página index
+    res.render("index");
+});
+
+//body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//database
+connection
+    .authenticate()
+    .then(()=> {
+        console.log("conexão com bd feita com sucesso!");
+    })
+    .catch((error) => {
+        console.error(erro);
+    });
+
+//inicializando servidor
+app.listen(8080, () => {
+    console.log("O servidor está rodando!");
 });
