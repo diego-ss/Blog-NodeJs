9e905e9a1af2b31c53eb304bde1c50885d6dafd2
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-28 23:25:26 -0300

    tela para adicionar categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,13 +2,10 @@ const express = require('express');
 //esse objeto permite declarar rotas sem aquela instanciação de app
 const router = express.Router();
 
-router.get("/categories", (req, res)=>{
-    res.send("ROTA DE CATEGORIAS");
+router.get("/admin/categories/new", (req, res)=>{
+    res.render("admin/categories/new")
 });
 
-router.get("/admin/categories/new", (req, res)=>{
-    res.send("ROTA DE CRIAÇÃO DE CATEGORIAS");
-});
 
 //depois é só exportar o router
 module.exports = router;
