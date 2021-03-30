de201f18f1dbad313e9591065c929e315b111db2
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-30 14:46:51 -0300

    edit artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -42,6 +42,25 @@ router.post("/articles/save", (req, res)=>{
     }
 });
 
+router.post("/articles/update", (req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+    var categoryId = req.body.category;
+    var article = req.body.body;
+
+    if(title != undefined){
+        Article.update({title: title, slug: slugify(title), body: article,categoryId: categoryId},
+            { 
+                where: {id:id}
+            })
+            .then(()=>{
+                res.redirect("/admin/articles");
+        });
+    } else {
+        res.redirect("/admin/articles/new");
+    }
+});
+
 router.post("/articles/delete", (req, res)=>{
     var id = req.body.id;
 
@@ -55,6 +74,32 @@ router.post("/articles/delete", (req, res)=>{
         res.redirect("/admin/articles");
     }
 });
+
+router.get("/admin/articles/edit/:id", (req, res)=>{
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/articles");
+    }
+
+    Article.findByPk(id).then(article =>{
+        if(article){
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", 
+                {
+                    article: article, categories:categories
+                });
+            })
+            
+        } else{
+            res.redirect("/admin/articles");
+        }
+    }).catch(error => {
+        res.redirect("/admin/articles");
+    });
+    
+});
+
 
 
 //depois é só exportar o router
