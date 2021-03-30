f7cb79fbd524c5991e5b177b50eda22b2ba984ca
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-30 12:38:45 -0300

    Finalizando adição de artigos

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('blognodejs', 'root', 'Diego!1996', {
     host:"localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    timezone: "-03:00" //ajustando timezone do banco de dados
 });
 
 module.exports = connection;
