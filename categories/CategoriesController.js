ac4b4e66b23689ed88ae36bdf05564e3514cb6f1
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-29 22:51:38 -0300

    Edição de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -24,6 +24,22 @@ router.post("/categories/save", (req, res)=>{
     }
 });
 
+router.post("/categories/update", (req, res)=>{
+    var id = req.body.id;
+    var title = req.body.title;
+
+    if(title != undefined){
+        Category.update({
+            title: title,
+            slug: slugify(title)
+        }, {where: {id:id}}).then(()=>{
+            res.redirect("/admin/categories");
+        });
+    } else {
+        res.redirect("/admin/categories/new");
+    }
+});
+
 router.post("/categories/delete", (req, res)=>{
     var id = req.body.id;
 
@@ -39,12 +55,32 @@ router.post("/categories/delete", (req, res)=>{
     }
 });
 
-
 router.get("/admin/categories", (req, res)=>{
 
     Category.findAll().then(categories => {
         res.render("admin/categories/index", 
         {categories: categories})
+    });
+    
+});
+
+router.get("/admin/categories/edit/:id", (req, res)=>{
+    var id = req.params.id;
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category =>{
+        if(category){
+            res.render("admin/categories/edit", {
+                category: category
+            });
+        } else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(error => {
+        res.redirect("/admin/categories");
     });
     
 });
