0d2def58585dac982f63d603755ea9faea46e6be
diego-ss <55899445+diego-ss@users.noreply.github.com>
2021-03-31 20:57:47 -0300

    Rota e tela de criação de usuários

--- users/UsersController.js
+++ users/UsersController.js
@@ -10,5 +10,19 @@ router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create");
 });
 
+router.post("/users/add", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.create({
+        email: email,
+        password: password
+    }).then(()=>{
+        res.redirect("/admin/users");
+    }).catch((error) => {
+        res.send(error);
+    });
+});
+
 //User.sync({force: true});
 module.exports = router;
